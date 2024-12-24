cb7b9cc031eecc2ff38272d890bebd91b55df843
Wellington Mendoza <wellsaint91@gmail.com>
2024-12-24 18:49:12 +0100

    feat: add create todo from db

--- routes/todos.js
+++ routes/todos.js
@@ -1,12 +1,25 @@
 var express = require("express");
 var router = express.Router();
 
-router.get("/", function (req, response, next) {
-  response.render("todos", { title: "Express" });
+const Todo = require("../models/todo");
+
+router.get("/", async function (req, response, next) {
+  const todos = await Todo.findAll();
+
+  response.render("todos", {
+    title: "Express",
+    todos: todos.map((todo) => ({ name: todo.taskName })),
+  });
 });
 
-router.post("/", function (req, response, next) {
-  response.render("todos", { title: req.body.task });
+router.post("/", async function (req, response, next) {
+  await Todo.create({ taskName: req.body.task });
+  const todos = await Todo.findAll();
+
+  response.render("todos", {
+    title: "FROM POST",
+    todos: todos.map((todo) => ({ name: todo.taskName })),
+  });
 });
 
 module.exports = router;
