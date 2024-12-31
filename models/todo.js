bdd155ddbd9043faad22680133d99a78ae1070ae
Wellington Mendoza <wellsaint91@gmail.com>
2024-12-31 17:46:45 +0100

    allow complete tasks

--- models/todo.js
+++ models/todo.js
@@ -7,6 +7,10 @@ const Todo = sequelize.define("Todo", {
     type: DataTypes.STRING,
     allowNull: false,
   },
+  completedAt: {
+    type: DataTypes.DATE,
+    allowNull: true,
+  },
 });
 
 module.exports = Todo;
--- routes/todos.js
+++ routes/todos.js
@@ -3,13 +3,21 @@ var router = express.Router();
 
 const Todo = require("../models/todo");
 
-router.get("/", async function (req, res, next) {
+const renderTodos = async (res, title) => {
   const todos = await Todo.findAll();
 
   res.render("todos", {
-    title: "Express",
-    todos: todos.map((todo) => ({ id: todo.id, name: todo.taskName })),
+    title,
+    todos: todos.map((todo) => ({
+      id: todo.id,
+      name: todo.taskName,
+      isCompleted: Boolean(todo.completedAt),
+    })),
   });
+};
+
+router.get("/", async function (req, res, next) {
+  renderTodos(res, "FROM GET");
 });
 
 // URL - Uniform Resource Locator
@@ -21,22 +29,23 @@ router.get("/", async function (req, res, next) {
 //  | PATCH  -> private | update (parcial)
 router.post("/", async function (req, res, next) {
   await Todo.create({ taskName: req.body.task });
-  const todos = await Todo.findAll();
 
-  res.render("todos", {
-    title: "FROM POST",
-    todos: todos.map((todo) => ({ id: todo.id, name: todo.taskName })),
-  });
+  renderTodos(res, "FROM POST");
 });
 
 router.delete("/", async function (req, res, next) {
   await Todo.destroy({ where: { id: req.body.id } });
-  const todos = await Todo.findAll();
 
-  res.render("todos", {
-    title: "FROM DELETE",
-    todos: todos.map((todo) => ({ name: todo.taskName })),
-  });
+  renderTodos(res, "FROM DELETE");
+});
+
+router.patch("/", async function (req, res, next) {
+  await Todo.update(
+    { completedAt: req.body.complete === "true" ? new Date() : null },
+    { where: { id: req.body.id } },
+  );
+
+  renderTodos(res, "FROM PATCH");
 });
 
 module.exports = router;
