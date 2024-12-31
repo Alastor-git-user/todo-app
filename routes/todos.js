657271741794dfc13c0ab2fd4a9c58c50c02d7fd
Wellington Mendoza <wellsaint91@gmail.com>
2024-12-31 17:28:11 +0100

    enable delete tasks

--- routes/todos.js
+++ routes/todos.js
@@ -3,21 +3,38 @@ var router = express.Router();
 
 const Todo = require("../models/todo");
 
-router.get("/", async function (req, response, next) {
+router.get("/", async function (req, res, next) {
   const todos = await Todo.findAll();
 
-  response.render("todos", {
+  res.render("todos", {
     title: "Express",
-    todos: todos.map((todo) => ({ name: todo.taskName })),
+    todos: todos.map((todo) => ({ id: todo.id, name: todo.taskName })),
   });
 });
 
-router.post("/", async function (req, response, next) {
+// URL - Uniform Resource Locator
+// HTML FORM: GET | POST
+// HTTP:
+//  | GET    -> public  | "get" data -> html | json
+//  | POST   -> private | create
+//  | DELETE -> private | delete
+//  | PATCH  -> private | update (parcial)
+router.post("/", async function (req, res, next) {
   await Todo.create({ taskName: req.body.task });
   const todos = await Todo.findAll();
 
-  response.render("todos", {
+  res.render("todos", {
     title: "FROM POST",
+    todos: todos.map((todo) => ({ id: todo.id, name: todo.taskName })),
+  });
+});
+
+router.delete("/", async function (req, res, next) {
+  await Todo.destroy({ where: { id: req.body.id } });
+  const todos = await Todo.findAll();
+
+  res.render("todos", {
+    title: "FROM DELETE",
     todos: todos.map((todo) => ({ name: todo.taskName })),
   });
 });
